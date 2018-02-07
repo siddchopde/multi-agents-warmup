9bcd4a652b30d3bfdc2a9ff5fc6be73b7e8411ca
mandersen21 <mikkelandersen21@gmail.com>
2018-02-07 15:20:08 +0100

    Commit

--- searchclient/Heuristic.java
+++ searchclient/Heuristic.java
@@ -1,7 +1,9 @@
 package searchclient;
 
 import java.util.Comparator;
-import java.util.Map;
+import java.util.*;
+
+import static java.lang.Math.abs;
 
 import static searchclient.Node.MAX_ROW;
 import static searchclient.Node.MAX_COL;
@@ -11,30 +13,28 @@ import searchclient.NotImplementedException;
 public abstract class Heuristic implements Comparator<Node> {
 	public Heuristic(Node initialState) {
 		// Here's a chance to pre-process the static parts of the level.
+
+		Map<String, String> goals = new HashMap<String, String>();
+		Map<String, String> boxes = new HashMap<String, String>();
 		
-		// Map<String, String> goals = new HashMap<String, String>();
-
-		// for (int row = 1; row < MAX_ROW - 1; row++) {
-		// 	for (int col = 1; col < MAX_COL - 1; col++) {
-		// 		char g = initialState.goals[row][col];
-		// 		if ('a' <= g && g <= 'z') {
-		// 			goals.put(new Integer(row).toString(), new Integer(col).toString());
-		// 		}
-		// 	}
-		// }
-
-		// System.out.println("length of goals" + goals.size());
+		
+		for (int row = 1; row < MAX_ROW - 1; row++) {
+			for (int col = 1; col < MAX_COL - 1; col++) {
+				char g = initialState.goals[row][col];
+				if ('a' <= g && g <= 'z') {
+					goals.put(String.valueOf(initialState.goals[row][col]), new Integer(row).toString() + "#" + new Integer(col).toString());
+				}
+				if ('A' <= g && g <= 'Z') {
+					boxes.put(String.valueOf(initialState.goals[row][col]), new Integer(row).toString() + "#" + new Integer(col).toString());
+				}
+			}
+		}
 	}
 
 	public int h(Node n) {
-
-		// if (numberGoals == 1) {
-		// 	dx = abs(n.agentCol - n.goals[0][1]);
-		// 	dy = abs(node.y - goal.y);
-		// 	return D * (dx + dy);
-		// } else {
-			return 0;
-		// }
+		int dx = abs(n.agentRow - goals.get("a").split());
+		int dy = abs(n.agentCol - goals.get("a")).split;
+		return 1 * (dx + dy);
 	}
 
 	public abstract int f(Node n);
